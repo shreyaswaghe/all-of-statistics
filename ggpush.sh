aca8e0b50b58fa0cd6e8bc740c96601ee7d5f4cb
shreyaswaghe <shreyaswaghe3@gmail.com>
2023-06-04 16:41:40 -0400

    Notes.now

---
+++ ggpush.sh
@@ -0,0 +1,22 @@
+# Shreyas's GitHub Push Shell Script
+
+echo "Pushing to GitHub"
+
+echo "What do you want to push?"
+echo "1) Notes"
+echo "2) Solutions"
+
+read NUM
+now = `date`
+
+if [ $NUM == 1 ]
+then
+    git add notes/
+    git commit -m "Notes".now
+elif [ $NUM == 2 ]
+then
+    git add solns/
+    git commit -m "Solutions".now
+fi
+
+git push
